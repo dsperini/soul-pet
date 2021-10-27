d33dc08fb6eb6057c32d31a573267ef7974d7c6b
Isabela Cardoso <isabela.cardoso@ibama.gov.br>
2021-10-27 19:06:17 -0300

    new e create de pets com vínculo ao usuário. botões de cadastrar pet e meus pets na nav bar

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,8 @@
 class PetsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show]
   before_action :set_pet, only: [ :show ]
+  before_action :pet_params, only: [ :create ]
+  # before_action :find_user
 
 
   def index
@@ -11,11 +13,42 @@ class PetsController < ApplicationController
     
   end
 
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    if @pet.valid?
+      @pet.save
+      redirect_to user_pet_pets_path
+    else
+      render :name
+    end
+  end
+
+  def user_pet
+    @user_id = current_user.id
+    @user = current_user
+    @pets = Pet.where(user_id: params[:user_id])
+  end
+
+
+
   private
 
   def set_pet
     @pet = Pet.find(params[:id])
   end
+
+  def pet_params
+    params.require(:pet).permit(:name, :age, :size, :breed, :status, :user_id)
+  end
+
+  # def find_user
+  #   @user = current_user
+  # end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  resources :pets
+  resources :pets do
+    collection do
+      get 'user_pet'
+    end
+  end
+  
   resources :adoptions
   
 end
